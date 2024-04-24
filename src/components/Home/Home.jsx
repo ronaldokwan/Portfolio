110305e6a53d8fb39e7f543e6cd2e5d8fd0ad03f
Ronaldo Kwan <ronaldokwan17@gmail.com>
2024-04-24 12:32:32 +0700

    animate fade in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,47 +7,63 @@ import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
-  const { ref, inView } = useInView({
-    triggerOnce: false, // Change this to false if you want the animation to trigger again whenever it comes in view
-    threshold: 0.5, // Adjust this value to control when the animation should trigger
+  const { ref: topSectionRef, inView: topSectionInView } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
+
+  const { ref: home2Ref, inView: home2InView } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
   });
 
   return (
     <section>
-      <Container fluid id="home">
+      <Container fluid className="home-section" id="home">
         <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
-              <h1 className="heading-name">
-                I&apos;M <strong className="main-name"> RONALDO KWAN</strong>
-              </h1>
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
-        </Container>
+        <div
+          className={`${
+            topSectionInView
+              ? "animate__animated animate__fadeIn"
+              : "animate__animated animate__fadeOut"
+          }`}
+          ref={topSectionRef}
+        >
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Hi There!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
+                    👋🏻
+                  </span>
+                </h1>
+                <h1 className="heading-name">
+                  I&apos;M <strong className="main-name"> RONALDO KWAN</strong>
+                </h1>
+                <div style={{ padding: 50, textAlign: "left" }}>
+                  <Type />
+                </div>
+              </Col>
+              <Col md={5} style={{ paddingBottom: 20 }}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </Container>
       <div
         className={`${
-          inView ? "animate__animated animate__fadeIn animate__delay-1s" : ""
+          home2InView
+            ? "animate__animated animate__fadeIn"
+            : "animate__animated animate__fadeOut"
         }`}
-        ref={ref}
+        ref={home2Ref}
       >
         <Home2 />
       </div>
