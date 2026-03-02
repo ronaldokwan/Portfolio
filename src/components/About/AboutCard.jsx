2bd83f747d53aea3e67ad8a19ae1ada5a559406c
ronaldokwan <ronaldokwan17@example.com>
2026-03-02 17:05:27 +1100

    Update AboutCard component to reflect current education and interests

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -8,6 +8,7 @@ import { useInView } from "react-intersection-observer";
 
 function AboutCard() {
   const { ref, inView } = useInView();
+
   return (
     <Card className="quote-card-view">
       <Card.Body
@@ -17,38 +18,42 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hello there, I&apos;m <span className="purple">Ronaldo Kwan</span>,
-            a passionate Full Stack Web Developer from{" "}
-            <span className="purple">Jakarta, Indonesia</span>. I recently
-            completed a{" "}
-            <span className="purple">Full Stack JavaScript Immersive</span>{" "}
-            Program at Hacktiv8 Coding Bootcamp, and I&apos;m thrilled to embark
-            on this exciting journey in the{" "}
+            a passionate <span className="purple">Full Stack Developer</span> and
+            currently a student at{" "}
+            <span className="purple">University of Technology Sydney (UTS)</span>.
+            I&apos;m excited to continue growing in the{" "}
             <span className="purple">tech industry</span>.
           </p>
+
           <p style={{ textAlign: "justify" }}>
             While coding is my true calling, I believe in nurturing a{" "}
             <span className="purple">well-rounded lifestyle</span>. When
             I&apos;m not immersed in code, you&apos;ll find me:
           </p>
+
           <ul className="about-activity-list">
             <li className="about-activity">
               <VscDebugBreakpointLog className="purple" /> Playing{" "}
               <span className="purple">Video Games</span> <FaGamepad />
             </li>
+
             <li className="about-activity">
               <VscDebugBreakpointLog className="purple" /> Exploring{" "}
               <span className="purple">Linux</span>, currently Fedora{" "}
               <SiFedora />
             </li>
+
             <li className="about-activity">
               <VscDebugBreakpointLog className="purple" /> Learning{" "}
               <span className="purple">Philosophy</span> <FaBookOpen />
             </li>
+
             <li className="about-activity">
               <VscDebugBreakpointLog className="purple" /> Playing{" "}
               <span className="purple">Piano</span> <FaMusic />
             </li>
-                 <li className="about-activity">
+
+            <li className="about-activity">
               <VscDebugBreakpointLog className="purple" /> Exercising{" "}
               <span className="purple">Physical Activity</span> <CgGym />
             </li>
